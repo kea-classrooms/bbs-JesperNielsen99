cb54f0d31cdc1bfeb419364f95b92edca5a89a2d
Jesper Nielsen <113165574+JesperNielsen99@users.noreply.github.com>
2023-02-06 13:30:35 +0000

    Commited everything

---
+++ fix-samples.sh
@@ -0,0 +1,9 @@
+cd samples/
+mv animals/countries/movies .
+mv animals/countries .
+
+mv animals/Monkies.txt animals/Monkeys.txt
+echo Monkeys > animals/Monkeys.txt
+mv movies/Germany.txt countries
+mv countries/.apes.txt animals
+cd -
